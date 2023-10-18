522c57f093a9c7c1494a1ff133e91d5982163f9d
Fernando Meireles <meirelesff@hotmail.com>
2023-10-17 21:57:11 -0300

    Rename params

--- R/rdd.R
+++ R/rdd.R
@@ -1,40 +1,46 @@
 #' RDD extensions for tidy
 #' 
-#' These functions provide extensions to the \code{modelsummary()} function for
-#' \code{rdd} objects created by the \code{rdrobust} package.
+#' Tidy methods for \code{rdd} objects created by the \code{rdrobust} package.
 #' 
 #' @param model An \code{rdd} object created by the \code{rdrobust} package.
 #' @param ... Additional arguments passed to \code{modelsummary()}.
 #' 
+#' @examples
+#' \dontrun{
+#' x <- runif(100, -10, 10)
+#' y <- 10 + 2*x + 2*(x>=0) + rnorm(100)
+#' mod <- rdrobust(y,x)
+#' 
+#' tidy(mod)
+#' glance(mod)
+#' }
+#' 
 #' @export 
 
-# Function to tidy the output of rdrobust
 tidy.rdrobust <- function(model, ...) {
-  ret <- data.frame(
+  res <- data.frame(
     term = row.names(model$coef)[3],
     estimate = model$coef[3],
     std.error = model$se[3],
     p.value = model$pv[3]
   )
-  row.names(ret) <- NULL
-  ret
+  row.names(res) <- NULL
+  res
 }
 
 
 #' RDD extensions for glance
 #' 
-#' These functions provide extensions to the \code{modelsummary()} function for
-#' \code{rdd} objects created by the \code{rdrobust} package.
+#' Glance methods for \code{rdd} objects created by the \code{rdrobust} package.
 #' 
-#' @param model An \code{rdd} object created by the \code{rdrobust} package.
-#' @param ... Additional arguments passed to \code{modelsummary()}.
+#' @inheritParams tidy.rdrobust
 #' 
 #' @export 
 
 glance.rdrobust <- function(model, ...) {
-  ret <- data.frame(
+  res <- data.frame(
     Janela = round(model$bws[1], 1),
     N = sum(model$N_h)
   )
-  ret
+  res
 }
